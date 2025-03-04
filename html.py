77e173fe264fad91f4f18049968d8caed7aa3dcf
X_Lv <3357891249@qq.com>
2025-03-04 10:39:32 +0800

    约饭

---
+++ html.py
@@ -0,0 +1,10 @@
+print("*************************************\n")
+m = int(input("请输入你选择的项目"))
+if m == 1:
+    print("hello")
+if m == 2:
+    print('你好')
+if m == 3:
+    print("约饭")
+else:
+    print("不嘻嘻")
